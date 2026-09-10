2e67a3ca9d70385614fe7dca6d1c6031a81f3480
davircarvalh0 <davirosendocarvalho@gmail.com>
2026-09-10 11:19:10 -0300

    começo do script

--- extracao/extracao_dados.py
+++ extracao/extracao_dados.py
@@ -1,2 +1,12 @@
 import pandas as pd
-from data_txt import 
+from data_txt import *
+import os
+
+pasta_xml = '.\data_xml'
+
+for data in pasta_xml.iterdir():
+    df = pd.read_xml()
+    chave = df[chaveAcesso]
+    valor = df[valorTotal]
+    df.to_csv('arquivo_final.csv', index=False)  
+
